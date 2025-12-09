ee8f584d6eaa27a8b1668267a30eb4c7279408ca
MarioP325 <mapantoja1@cps.edu>
2025-12-09 20:43:38 +0000

    11073 loops

--- 1_loops.py
+++ 1_loops.py
@@ -1,7 +1,7 @@
 # Example Practice:
 # Given this list of fruits:
 fruits = ["apple", "banana", "cherry", "date"]
-
+print(len(fruits))
 # Challenge:
 # Use a for loop to print each fruit on a new line.
 print(fruits[0])
@@ -13,8 +13,15 @@ for fruit in fruits:
 
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
+print(len(subjects))
 for subject in subjects:
-    print(subject)
+    if subject == subjects[3]:
+        break
+    else:
+        print(subject)
+    
+for index in range(len(subjects)):
+    print("subject" + str(index) + ":" + subjects[index])
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
@@ -27,6 +34,13 @@ numbers = [5, 10, 15, 20]
 # Use a for loop to add all the numbers and print the total.
 total=0
 for number in numbers:
-    print(number)
     total += number
     print(total)
+    print( "total", total)
+    break
+
+new_numbers= list(range(1,600001))
+ntotal=0
+for new in new_numbers:
+    ntotal += new
+print(ntotal)
